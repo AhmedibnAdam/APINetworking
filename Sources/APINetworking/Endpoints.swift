38648ebaf2dd2bfde327ecf6d78356862f07e75e
AhmedibnAdam <ibn.abuadam@gmail.com>
2024-10-30 19:23:41 +0300

    init

---
+++ Sources/APINetworking/Endpoints.swift
@@ -0,0 +1,12 @@
+import Foundation
+import APINetworking
+
+public extension Endpoint {
+  static let twitter = Endpoint(path: "/api/character")
+}
+
+public enum Endpoints {
+  public static let criticalmassInEndpoint = "criticalmass.in"
+  public static let apiEndpoint = "rickandmortyapi.com"
+    /// "https://rickandmortyapi.com/api"
+}
